860e6a5a90803c8c117ceba817108cefadabb624
Nirob-Barman <niroborin111@gmail.com>
2024-05-04 17:34:47 +0600

    temp data

---
+++ RazorSyntax/RazorSyntax/Controllers/TempDataController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RazorSyntax.Controllers
+{
+    public class TempDataController : Controller
+    {
+        // GET: TempData
+        public ActionResult Index()
+        {
+            TempData["myKey"] = "Data from index metod";
+            TempData.Keep("myKey");
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            Session.Abandon();
+
+            ViewBag.myKey = TempData["myKey"];
+            TempData.Keep("myKey");
+            //ViewBag.myKey = TempData.Peek("myKey");
+            return View();
+        }
+        public ActionResult Index3()
+        {
+            ViewBag.myKey = TempData["myKey"];
+            return View();
+        }
+    }
+}
