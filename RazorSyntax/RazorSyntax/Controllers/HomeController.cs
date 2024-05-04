4246a4ba0a4a1344ab7577e7206ecf15d9f9c40b
Nirob-Barman <niroborin111@gmail.com>
2024-05-04 16:42:03 +0600

    View bag and data

--- RazorSyntax/RazorSyntax/Controllers/HomeController.cs
+++ RazorSyntax/RazorSyntax/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RazorSyntax.Models;
 
 namespace RazorSyntax.Controllers
 {
@@ -11,6 +12,7 @@ namespace RazorSyntax.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            ViewBag.myName = "Nirob Barman";
             return View();
         }
         public ActionResult About(string name = null)
@@ -18,7 +20,23 @@ namespace RazorSyntax.Controllers
             ViewBag.Name = name;
             //var abc = "Name Parameter in About View: " + name;
             Console.WriteLine("Name Parameter in About View: " + name);
+            ViewBag.myList = new List<string>()
+            {
+                "John", "Doe", "kim", "Rock"
+            };
+            List<ViewBagEmployee> employeeList = new List<ViewBagEmployee>
+            {
+                new ViewBagEmployee() { Id = 1, Name = "Abul", Email = "abul@gmail.com" },
+                new ViewBagEmployee() { Id = 2, Name = "Babul", Email = "babul@gmail.com" },
+                new ViewBagEmployee() { Id = 3, Name = "Cabul", Email = "cabul@gmail.com" },
+                new ViewBagEmployee() { Id = 4, Name = "Dabul", Email = "dabul@gmail.com" },
+            };
+            
+            ViewData["MyKey"] = "This is a value for from view data";
+            ViewData["myList"] = new List<string>(){"John", "Doe", "kim", "Rock"};
+            ViewBag.employeeList = employeeList;
             return View();
         }
+
     }
 }
