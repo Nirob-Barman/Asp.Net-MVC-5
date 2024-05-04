5b39ff5848d2a86621223398c03c394a0fc8a381
Nirob-Barman <niroborin111@gmail.com>
2024-05-04 14:20:42 +0600

    Templated HTML helpers

---
+++ RazorSyntax/RazorSyntax/Controllers/TemplatedHelperController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RazorSyntax.Models;
+
+namespace RazorSyntax.Controllers
+{
+    public class TemplatedHelperController : Controller
+    {
+        // GET: TemplatedHelper
+        public ActionResult Index()
+        {
+            TemplateEmployee emp = new TemplateEmployee() {
+                ID = 1,
+                Name = "Nirob Barman",
+                Email = "nirob@gmail.com",
+                isOnline = false,
+                DateOfBirth = DateTime.Now
+            };
+            return View(emp);
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(TemplateEmployee emp)
+        {
+
+            return View();
+        }
+    }
+}
---
+++ RazorSyntax/RazorSyntax/Models/TemplateEmployee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorSyntax.Models
+{
+    public class TemplateEmployee
+    {
+        public int ID { get; set; }
+        [Display(Name="Please Enter your name")]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool isOnline { get; set; }
+        [Display(Name="Date of Birth")]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+        [DataType(DataType.Time)]
+        public DateTime BirthTime { get; set; }
+    }
+}
